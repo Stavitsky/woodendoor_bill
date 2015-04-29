ba54c12b18041edad9d2432008d31c9c72e31cf7
Stavitsky A <alexandr.stavitsky@gmail.com>
2015-04-29 12:41:46 +0300

    fixed new day hour count

--- woodendoor_bill.py
+++ woodendoor_bill.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import datetime
 
-COMING_TIME = datetime.time()
+COMING_TIME = datetime.time(12, 15)
+
 
 def count_time(coming_time):
-	leaving_time = datetime.datetime.time(datetime.datetime.now())
-	hours = leaving_time.hour - COMING_TIME.hour
-	minutes = leaving_time.minute - COMING_TIME.minute
-	spent_time = {
-	    'hours': hours,
-	    'minutes': minutes
-	}
-	return spent_time
+    leaving_time = datetime.datetime.time(datetime.datetime.now())
+    spent_hours = leaving_time.hour - COMING_TIME.hour
+    spent_minutes = leaving_time.minute - COMING_TIME.minute
+    spent_time = {
+        'hours': spent_hours if spent_hours >= 0 else 24+spent_hours,
+        'minutes': spent_minutes
+    }
+    return spent_time
+
 
 def count_cost(spent_time):
-	if spent_time['hours'] >= 2:
-		cost = 240 + (spent_time['hours']-2) * 60 + spent_time['minutes']
-	else:
-		cost = spent_time['hours'] * 60 * 2 + spent_time['minutes'] * 2  	
-	return cost
+    if spent_time['hours'] >= 2:
+        cost = 240 + (spent_time['hours'] - 2) * 60 + spent_time['minutes']
+    else:
+        cost = spent_time['hours'] * 60 * 2 + spent_time['minutes'] * 2
+    return cost
+
 
 def print_report(spent_time, cost):
-	print "Вы провели в кафе:\n\t{0} час(ов),\n\t{1} минут.\n\nC Вас {2} рублей.".format(
-		spent_time['hours'], spent_time['minutes'], cost)
+    print ("You had spent at WoodenDoor:\n\t{0} hour(s),"
+           "\n\t{1} minute(s).\n\nYou owe {2} rubles.").format(
+            spent_time['hours'], spent_time['minutes'], cost)
+
 
 def main():
-	spent_time = count_time(COMING_TIME)
-	cost = count_cost(spent_time)
-	print_report(spent_time, cost)
+    spent_time = count_time(COMING_TIME)
+    cost = count_cost(spent_time)
+    print_report(spent_time, cost)
 
 if __name__ == "__main__":
     main()
